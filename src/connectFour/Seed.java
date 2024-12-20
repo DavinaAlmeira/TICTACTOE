4dd81f98501f9a9d48f06bf521f76da41a61ce77
haiibuddyyy <5026231023@student.its.ac.id>
2024-12-21 00:16:28 +0700

    Add identitas kelompok

--- src/connectFour/Seed.java
+++ src/connectFour/Seed.java
@@ -1,4 +1,14 @@
 /**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #8
+ * 1 - 5026231023 - Nadya Luthfiyah Rahma
+ * 2 - 5026231094 - Davina Almeira
+ * 3 - 5026231148 - Tiara Aulia Azadirachta Indica
+ */
+
+/**
  * This enum is used by:
  * 1. Player: takes value of CROSS or NOUGHT
  * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
